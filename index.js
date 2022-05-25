e43c2bcd94cdd65d51a9d83dbe010a96eb9932f5
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-26 01:44:54 +0600

    product tools api created for post

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const res = require('express/lib/response');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +10,32 @@ const port = process.env.PORT || 5000;
 // Middlewire
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfcm7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('mediTools db connected');
+        const ToolCollection = client.db('mediToolsDB').collection('product');
+
+        // tools product api
+        app.post('/product', async (req, res) => {
+            const query = req.body;
+            const product = await ToolCollection.insertOne(query);
+            res.send(product);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
