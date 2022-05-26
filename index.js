1a7648810414249efebdd2ffb487ec8a692baaae
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-26 19:22:29 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const res = require('express/lib/response');
 
 const app = express();
 const port = process.env.PORT || 5000;
